e36a3dce3554976cc308b3e54132f0b05fd59501
Sojeong Lee <sojeong.lee017@gmail.com>
2022-11-02 17:18:31 +0900

    과제 5

--- 2022-Java/Assignments/Project5/src/Project05.java
+++ 2022-Java/Assignments/Project5/src/Project05.java
@@ -15,22 +15,28 @@ class AccountMenu {
         account2 = new BankAccount(n2, a2, b2);
         go();
     }
-       
+    
+    // 한 번에 모두 실행
     private void go() {
         showAccount();
         showMenu();
+        doMenu();
     }
+
+    // 계좌 정보 출력
     private void showAccount() {
         System.out.println("Account1: ");
         account1.showInfo();
         System.out.println("Account2: ");
         account2.showInfo();
     }
+    // 계좌 메뉴 출력
     private void showMenu() {
         System.out.print(
                 "1. Deposit A1   2. Deposit A2   3. Withdraw A1   4. Withdraw A2   5. Transfer A1->A2   6. Transfer A2->A1   0.Exit : ");
-        setMenu(sc.nextInt());        
+        setMenu(sc.nextInt());
     }
+    // 메뉴 입력값 handle
     private void setMenu(int n){
         if (n >= 0 && n <= 6) {
             menu = n;
@@ -39,16 +45,15 @@ class AccountMenu {
             System.out.println("Error: enter a right number");
             showMenu();
         }
-        doMenu();
     }
 
-
+    // 메뉴 실행
     private void doMenu() {
         if(menu == 0){
             sc.close(); // do nothing
             return;
         }
-        amount = getAmount();
+        amount = setAmount();
         if(menu == 1) {            
             account1.deposit(amount);
         }
@@ -67,10 +72,12 @@ class AccountMenu {
         else {
             account2.bankTransfer(account1, amount);
         }
+        // 메뉴 실행 후 무한반복
         System.out.println();
         go();
     }
-    private int getAmount(){
+    // 사용자로부터 금액 입력
+    private int setAmount(){
         System.out.print("How much do you want [menu"+ menu + "] ");
         return sc.nextInt();
     }
@@ -86,34 +93,30 @@ class BankAccount {
         account = a;
         balance = b;
     }
-
     public void showInfo() {
         System.out.println(" Name: " + name +
                 ", Account: " + account +
                 ", Balance: " + balance);
     }
-
     public void bankTransfer(BankAccount other, int amount) {
         withdraw(amount);
         other.deposit(amount);
     }
-
     public void deposit(int amount) {
         balance += amount;
     }
-
     public void withdraw(int amount) {
-        if (balance >= amount) {
+        if (balance >= amount) {     
             balance -= amount;
-        } else {
+        } else {                // 잔액 >= 출금액
             System.out.println("Balance is not enough");
             System.out.println("Current balance: " + balance);
         }
     }
 }
 
-public class Ch8practice03 {
-    public static void main(String[] args) throws Exception {
+public class Project05 {
+    public static void main(String[] args) {
         AccountMenu am = new AccountMenu("Hong", "111-1234", 10000, "Kim", "111-5678", 2000);
     }
 }
